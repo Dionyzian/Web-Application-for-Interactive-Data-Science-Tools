bc926b5f6ae0c3bab23cd53edec8c49569b8ffde
Dionyzian <adlene.beddiar@outlook.com>
2023-03-14 23:04:32 +0100

    changed BarChart library for ApexCharts

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,22 +1,6 @@
-import React, { useCallback } from 'react';
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import { useCurrentPng } from 'recharts-to-png';
-import { saveAs } from 'file-saver';
-import { ReactComponent as DownloadIcon } from '../assets/download-icon.svg'
+import ReactApexChart from "react-apexcharts"
 
-
-const BarChartComponent = ({ data, nominalFeature }) => {
-  const [getPng, { ref, isLoading }] = useCurrentPng();
-
-  const handleDownload = useCallback(async () => {
-    const png = await getPng();
-
-    if (png) {
-      saveAs(png, 'myChart.png');
-    }
-  }, [getPng]);
-
-
+const BarChart = ({ data, nominalFeature }) => {
   const counts = {};
   data.forEach((d) => {
     const value = d[nominalFeature];
@@ -24,31 +8,38 @@ const BarChartComponent = ({ data, nominalFeature }) => {
   });
   const dataCounts = Object.keys(counts).map((key) => ({ name: key, count: counts[key] }));
 
+  let series = dataCounts.map((row) => {
+    return row.count
+  })
+
+  series = [{ data: series }]
+
+  const options = {
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: dataCounts.map((row) => {
+        return row.name
+      }),
+    }
+  }
+
   return (
-    <>
-      <div className='relative'>
-        <DownloadIcon onClick={handleDownload} className="absolute top-[25px] right-[17%] z-50 download-icon" />
-        <ResponsiveContainer width="90%" height={350}>
-          <BarChart ref={ref} data={dataCounts} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="name"
-              interval={0}
-              angle={-45}
-              textAnchor='end'
-              height={85}
-              tick={{ dx: 15 }}
-            />
-            <YAxis />
-            <Tooltip />
+    <div id="chart">
+      <ReactApexChart options={options} series={series} type="bar" height={350} />
+    </div>
+  )
+}
 
-            <Bar dataKey="count" fill="#8884d8" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
-    </>
-
-  );
-};
-
-export default BarChartComponent;
+export default BarChart
